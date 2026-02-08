58f691387f61dd2eecf5a34f32e61e4547b81b01
정희우 <57035369+huiwoo1010@users.noreply.github.com>
2026-02-08 17:18:22 +0900

    init

---
+++ app/src/main/java/com/example/snutiexp/login/LoginActivity.kt
@@ -0,0 +1,6 @@
+package com.example.snutiexp.login
+
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+}
---
+++ app/src/main/java/com/example/snutiexp/signup/SignupActivity.kt
@@ -0,0 +1,6 @@
+package com.example.snutiexp.signup
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SignupActivity : AppCompatActivity() {
+}
